2c5f30e8698ec222c6b32240c8792e424c7a5abc
jatorrico <37845485+jatorrico@users.noreply.github.com>
2018-05-15 00:51:06 -0300

    Create io.h

---
+++ .gitignore/io.h
@@ -0,0 +1,7 @@
+/*mensajes*/	
+
+#define ERR_MSG_NULL_PTR "Error puntero nulo"
+#define ERR_MSG_ARGS "Los argumentos ingresados no son validos"
+#define ERR_MSG_AMOUNT_OF_ARGS "La cantidad de argumentos no es valida"
+#define ERR_MSG_SIZE "El tamaño asignado no valido"
+#define ERR_MSG_OUT_OF_MEMORY "Memoria insufuciente"
